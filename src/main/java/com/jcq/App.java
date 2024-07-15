0c8fbf3dcd5827be96f4023506ad1d15574cba2a
alfonsoloplaf <alfloplaf@gmail.com>
2024-07-15 18:54:42 +0200

    Novela renderizada con lorem

--- src/main/java/com/jcq/App.java
+++ src/main/java/com/jcq/App.java
@@ -1,12 +1,77 @@
 package com.jcq;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 
 public class App 
 {
     public static void main(String[] args) {
         
-        GameScreen.init();
-        GameScreen.kill();
+        try {
+            GameScreen.init();
+
+            //Cuadros de novela
+            GameScreen.drawSeparatingRectangle(new TerminalPosition(1, 27), new TerminalSize(32, 17));
+            GameScreen.drawSeparatingRectangle(new TerminalPosition(33, 27), new TerminalSize(68, 17));
+            GameScreen.drawSeparatingRectangle(new TerminalPosition(101, 27), new TerminalSize(32, 17));
+            GameScreen.drawSeparatingRectangle(new TerminalPosition(1, 1), new TerminalSize(132, 26));
+
+            GameScreen.refresh();
+
+            readFile("");
+            GameScreen.nextPage();
+            GameScreen.kill();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void readFile(String fileName) throws InterruptedException{
+        //Todo tuyo
+        String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue. Elit eget gravida cum sociis natoque penatibus. Lectus urna duis convallis convallis tellus id interdum velit. Faucibus vitae aliquet nec ullamcorper sit amet risus. Molestie ac feugiat sed lectus vestibulum mattis. Sed elementum tempus egestas sed. Ac tortor vitae purus faucibus ornare suspendisse. Purus faucibus ornare suspendisse sed nisi lacus sed viverra. Semper feugiat nibh sed pulvinar proin gravida hendrerit. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit. Lectus urna duis convallis convallis tellus id interdum velit. Nec ullamcorper sit amet risus nullam eget. Venenatis urna cursus eget nunc scelerisque. Lectus arcu bibendum at varius vel pharetra vel turpis. In iaculis nunc sed augue.";
+
+        GameScreen.printPortrait(0, "jean/src/main/java/com/jcq/Jean.txt");
+        GameScreen.printPortrait(1, "jean/src/main/java/com/jcq/Moscardina.txt");
+
+        String nameJ = "Jean";
+        String nameM = "Moscardina";
+
+        GameScreen.drawSeparatingRectangle(new TerminalPosition(35, 28), new TerminalSize(nameJ.length()+2, 3));
+        GameScreen.drawSeparatingRectangle(new TerminalPosition(100-nameM.length()-3, 28), new TerminalSize(nameM.length()+2, 3));
+        GameScreen.print(nameJ, 36, 29);
+        GameScreen.print(nameM, 100-nameM.length()-2, 29);
+
+        int line = 32;
+        GameScreen.setCursor(35, line);
+        int screenLimit = 99;
+
+        String[] words = lorem.split(" ");
+        for (String word : words) {
+            
+            if (word.length() > screenLimit - GameScreen.getCursorX()) {
+                if (line == 41) {
+                    GameScreen.nextPage();
+                    line = 32;
+                    GameScreen.setCursor(35, line);
+                }
+                else {
+                    line++;
+                    GameScreen.setCursor(35, line);
+                }
+            }
+
+            for (int i = 0; i < word.length(); i++) {
+                GameScreen.print(word.charAt(i));
+                GameScreen.refresh();
+                Thread.sleep(20);
+            }
+            GameScreen.nextXpos();
+        }
 
     }
 }
+
--- src/main/java/com/jcq/GameScreen.java
+++ src/main/java/com/jcq/GameScreen.java
@@ -1,8 +1,13 @@
 package com.jcq;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
@@ -26,7 +31,9 @@ public final class GameScreen {
             terminal.clearScreen();
             terminal.setCursorVisible(false);
 
-            textGraphics =terminal.newTextGraphics();
+            textGraphics = terminal.newTextGraphics();
+            textGraphics.drawRectangle(new TerminalPosition(0, 0), new TerminalSize(134, 45), '#');
+            terminal.flush();
         } 
         catch (IOException e) {
             if(terminal != null)
@@ -80,8 +87,72 @@ public final class GameScreen {
         textGraphics.putString(new TerminalPosition(x, y), s);
     }
 
+    public static void drawSeparatingRectangle(TerminalPosition topLeft, TerminalSize size) {
+        String s = "╔";
+        for (int i = 0; i < size.getColumns()-2; i++) s+="═";
+        s += "╗";
+        textGraphics.putString(topLeft, s);
+
+        s = "╚";
+        for (int i = 0; i < size.getColumns()-2; i++) s+="═";
+        s += "╝";
+
+        textGraphics.putString(topLeft.withRelativeRow(size.getRows()-1), s);
+
+        TerminalPosition topRight = new TerminalPosition(topLeft.getColumn()+size.getColumns()-1, topLeft.getRow());
+        textGraphics.drawLine(topLeft.withRelativeRow(1), topLeft.withRelativeRow(size.getRows()-2), '║');
+        textGraphics.drawLine(topRight.withRelativeRow(1), topRight.withRelativeRow(size.getRows()-2), '║');
+    }
+    
+    public static void printPortrait(int direction, String filePath) {
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+            String line;
+            int col = 0;
+            int row = 28;
+
+            if (direction == 0) col = 2;
+            if (direction == 1) col = 103;
+
+            while ((line = br.readLine()) != null) {
+
+                textGraphics.putString(new TerminalPosition(col, row), line);
+                row++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void nextPage() {
+        try {
+            KeyStroke keyStroke = terminal.readInput();
+            while(keyStroke.getKeyType() != KeyType.Enter) {keyStroke = terminal.readInput();}
+
+            textGraphics.fillRectangle(new TerminalPosition(35, 32), new TerminalSize(64, 10), ' ');
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     //Para luego hacerlo
     public static boolean isConsoleSizeSufficient() {return true;}
+
+    public static void refresh() {
+        try {
+            if (isConsoleSizeSufficient()) terminal.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int getCursorX (){
+        try {
+            return terminal.getCursorPosition().getColumn();
+        }
+        catch (IOException e) {return -1;}
+    }
 
     public static void kill() {
         try {
@@ -91,3 +162,4 @@ public final class GameScreen {
     }
 }
 
+                             
